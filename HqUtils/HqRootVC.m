1fe75fe4591f28495ab4322845565119c3841b72
macpro <>
2018-01-25 09:56:40 +0800

    Rename

---
+++ HqUtils/HqRootVC.m
@@ -0,0 +1,72 @@
+//
+//  ViewController.m
+//  HqUtils
+//
+//  Created by macpro on 2018/1/24.
+//  Copyright © 2018年 macpro. All rights reserved.
+//
+
+#import "HqRootVC.h"
+#import "HqColorImage.h"
+@interface HqRootVC ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong) UITableView *tableView;
+@property (nonatomic,strong) NSArray *titles;
+
+@end
+
+@implementation HqRootVC
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    _titles = @[@"HqColorImage"];
+    [self initView];
+}
+- (void)initView{
+    _tableView = [[UITableView alloc]initWithFrame:self.view.frame style:UITableViewStyleGrouped];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return _titles.count;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 50;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellIndentfier = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIndentfier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIndentfier];
+    }
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.textLabel.text = _titles[indexPath.row];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+
+}
+- (void)hqBtn{
+    UIImage *normalImage = [HqColorImage imageWithColor:[UIColor blueColor] isBorder:YES];
+    UIImage *HighlightedImage = [HqColorImage imageWithColor:[UIColor blueColor] isBorder:NO];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitle:@"Title" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateHighlighted];
+    [btn setBackgroundImage:normalImage forState:UIControlStateNormal];
+    [btn setBackgroundImage:HighlightedImage forState:UIControlStateHighlighted];
+    btn.frame = CGRectMake(10, 80, 100, 40);
+    [self.view addSubview:btn];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
