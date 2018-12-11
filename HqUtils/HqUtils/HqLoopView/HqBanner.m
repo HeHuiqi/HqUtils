103ebea84f7d4004334ed3c153dbe4f250246928
hhq <huiqi.he@solarsoft.org>
2018-12-11 11:29:26 +0800

    HqLoopView

---
+++ HqUtils/HqUtils/HqLoopView/HqBanner.m
@@ -0,0 +1,13 @@
+//
+//  HqBanner.m
+//  GlobalPay
+//
+//  Created by hqmac on 2018/9/3.
+//  Copyright © 2018年 solar. All rights reserved.
+//
+
+#import "HqBanner.h"
+
+@implementation HqBanner
+
+@end
